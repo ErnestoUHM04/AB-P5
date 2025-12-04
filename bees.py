04584b74295a40bc8ce819bb5557309fe2a6257a
ErnestoUlisesHM <netito.uli@icloud.com>
2025-12-03 20:09:18 -0600

    Initial commit

---
+++ bees.py
@@ -0,0 +1,14 @@
+# Cornejo Morales Paola
+# Hernándz Martínez Ernesto Ulises
+
+# Usar el algoritmo de colonia de abejas para resolver el problema de la mochila que se asignó en la práctica 1.
+# Recuerden utilizar los límites de las variables de acuerdo con problema, respetando los límites de 3 Love Potions y 2 Skiving Snackbox.
+# Para el resto de productos los límites están entre (0,10).
+
+# Tamaño del enjambre = 40
+# abeja obreras = 20
+# abejas Observadoras = 20
+# límite = 5
+# iteraciones = 50
+
+import random
